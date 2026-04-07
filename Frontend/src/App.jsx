841416f0f000a475cde982a9de701a4e7bf1b0bd
Toaa Mahmoud <toaamahmoud1642003@gmail.com>
2026-04-08 01:33:48 +0200

    feat: add vercel json for routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import AuthPage from './pages/AuthPage'
 import {Toaster} from 'react-hot-toast'
 function App() {
   return (
     <div>
-      <Toaster position='top-right'/>
+      <Toaster 
+        position='top-right' 
+        toastOptions={{
+            style: {
+                background: '#1a1a1a',
+                color: '#fff',
+                borderRadius: '4px',
+                fontSize: '14px',
+            }
+        }}
+      />
       <Routes>
+        <Route path="/" element={<Navigate to="/auth" />} />
         <Route path='/auth' element={<AuthPage/>}></Route>
       </Routes>
     </div>
